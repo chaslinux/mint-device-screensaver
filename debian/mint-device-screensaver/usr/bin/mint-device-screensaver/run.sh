5fd7cd3261d989ab89495cf2015b16c74b1940bd
chaslinux <chaslinux@gmail.com>
2026-08-05 17:13:19 -0400

    Remove Debian build artifacts from repository

--- debian/mint-device-screensaver/usr/bin/mint-device-screensaver/run.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-exec python3 /usr/share/mint-device-screensaver/main.py "$@"
--- debian/mint-device-screensaver/usr/share/mint-device-screensaver/test_icon.py
+++
@@ -1,6 +0,0 @@
-from giimports import Rsvg
-from pathlib import Path
-
-from icon_actor import IconActor
-
-print("Icon actor loaded successfully")
