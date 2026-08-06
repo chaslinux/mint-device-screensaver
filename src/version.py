ea2523d574371943ff31767b4884de30694d6a87
chaslinux <chaslinux@gmail.com>
2026-08-06 13:19:18 -0400

    Prepare v0.1.2 release

--- src/version.py
+++ src/version.py
@@ -5,5 +5,5 @@ Keeping this in a separate file means the version only needs to be
 updated in one place before building a Debian package.
 """
 
-VERSION = "0.1.0"
+VERSION = "0.1.2"
 APP_NAME = "Mint Device Screensaver"
