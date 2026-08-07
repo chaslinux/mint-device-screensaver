d04a1665fb1079486c3769ed50001ef4cef0c47d
chaslinux <chaslinux@gmail.com>
2026-08-07 12:08:19 -0400

    Bump version to 0.10.2

--- src/version.py
+++ src/version.py
@@ -5,5 +5,5 @@ Keeping this in a separate file means the version only needs to be
 updated in one place before building a Debian package.
 """
 
-VERSION = "0.10.1"
+VERSION = "0.10.2"
 APP_NAME = "Mint Device Screensaver"
